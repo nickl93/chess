95194fd283e2cdaf45546a7cd86d022d8de07c80
Nicholas Lambourne <ndl93@live.com>
2018-04-25 18:33:13 -0400

    added crayon support

--- chess.py
+++ chess.py
@@ -1,12 +1,134 @@
 from abc import ABCMeta, abstractmethod
+import crayons
+
+
+class Piece(metaclass=ABCMeta):
+    def __init__(self, x, y, direction, owner):
+        self.in_play = True
+        self.type = None
+        self.repr = None
+        self.x = x
+        self.y = y
+        self.owner = owner
+        self.direction = direction
+        self.moves = 0
+
+    def colored_str(self):
+        if self.owner == 1:
+            return crayons.white(self.repr, bold=True)  # White is black?
+        else:
+            return crayons.black(self.repr, bold=True)  # Black is white?
+
+    @abstractmethod
+    def valid_move(self, target_x, target_y, current_player):
+        if current_player != self.owner:
+            print("Is that your piece?")
+            return False
+        if self.x == target_x and self.y == target_y:
+            return False
+
+
+class Player:
+    def __init__(self, colour):
+        self.colour = colour
+        self.captured_pieces = list()
+
+
+class Pawn(Piece):
+    def __init__(self, x, y, direction, owner):
+        super().__init__(x, y, direction, owner)
+        self.type = "Pawn"
+        self.repr = "P"
+
+    def valid_move(self, target_x, target_y, current_player):
+        super().valid_move(target_x, target_y, current_player)
+        if target_y != self.y + self.direction and \
+                (target_y != self.y + 2 * self.direction and self.moves == 0):
+            return False
+        if target_x > self.x + 1 or target_x < self.x - 1:
+            return False
+        # Straight move
+        return True
+
+
+class King(Piece):
+    def __init__(self, x, y, direction, owner):
+        super().__init__(x, y, direction, owner)
+        self.type = "King"
+        self.repr = "K"
+
+    def valid_move(self, target_x, target_y, current_player):
+        super().valid_move(target_x, target_y, current_player)
+        if self.x - 1 <= target_x <= self.x + 1 and self.y - 1 <= target_y <= self.y + 1:
+            return True
+        else:
+            # Check for castling
+            return False
+
+
+class Queen(Piece):
+    def __init__(self, x, y, direction, owner):
+        super().__init__(x, y, direction, owner)
+        self.type = "Queen"
+        self.repr = "Q"
+
+    def valid_move(self, target_x, target_y, current_player):
+        super().valid_move(target_x, target_y, current_player)
+        if (target_x - self.x) % (target_y - self.y) == 0 or \
+            target_y == self.y or \
+                target_x == self.x:
+            return True
+        else:
+            return False
+
+
+class Bishop(Piece):
+    def __init__(self, x, y, direction, owner):
+        super().__init__(x, y, direction, owner)
+        self.type = "Bishop"
+        self.repr = 'B'
+
+    def valid_move(self, target_x, target_y, current_player):
+        super().valid_move(target_x, target_y, current_player)
+        if (target_x - self.x) % (target_y - self.y) == 0:
+            return True
+        else:
+            return False
+
+
+class Knight(Piece):
+    def __init__(self, x, y, direction, owner):
+        super().__init__(x, y, direction, owner)
+        self.type = "Knight"
+        self.repr = "K"
+
+    def valid_move(self, target_x, target_y, current_player):
+        super().valid_move(target_x, target_y, current_player)
+        if (abs(target_x - self.x) == 2 and abs(target_y - self.y) == 3) or \
+                (abs(target_x - self.x) == 3 and abs(target_y - self.y) == 3):
+            return True
+        else:
+            return False
+
+
+class Rook(Piece):
+    def __init__(self, x, y, direction, owner):
+        super().__init__(x, y, direction, owner)
+        self.type = "Rook"
+        self.repr = "R"
+
+    def valid_move(self, target_x, target_y, current_player):
+        super().valid_move(target_x, target_y, current_player)
+        if target_y == self.y or target_x == self.x:
+            return True
+        else:
+            return False
 
 
 class Board:
-    size = None
-    places = list()
-    game = None
 
     def __init__(self, parent_game):
+        self.places = list()
         self.sizeX = 8
         self.sizeY = 8
         self.populate_board()
@@ -56,24 +178,22 @@ class Board:
         for y in range(self.sizeY):
             print(str(y) + " ", end="")
             for x in range(self.sizeX):
-                if x != self.sizeX - 1:
-                    print(self.stringify(self.places[x][y]) + "|", end="")
+                if x == self.sizeX - 1:
+                    print(self.stringify(self.places[x][y]), end="")
                 else:
                     print(self.stringify(self.places[x][y]), end="")
+                    print('|', end="")
             print("")
 
-    def stringify(self, value):
+    @staticmethod
+    def stringify(value):
         if value is None:
             return "_"
         else:
-            return str(value)
+            return value.colored_str()
 
     def get_piece(self, x, y):
         return self.places[x][y]
-
-    """
-    Assumes coordinates have already been verified.
-    """
 
     def attempt_move(self, coordinates):
         piece_x = coordinates[0]
@@ -82,7 +202,7 @@ class Board:
         target_y = coordinates[3]
         piece_to_move = self.get_piece(piece_x, piece_y)
         if piece_to_move is not None:
-            if piece_to_move.valid_move(target_x, target_y, game.current_player):
+            if piece_to_move.valid_move(target_x, target_y, self.game.current_player):
                 captured_piece = self.get_piece(target_x, target_y)
                 if captured_piece is not None and captured_piece.owner != game.current_player:
                     captured_piece.in_play = False
@@ -94,142 +214,11 @@ class Board:
                                                                                  target_y))
                     if self.get_piece(target_x, target_y).type == "King":
                         self.game.is_over = True
-                self.places[target_x][target_y] = piece_to_move
-                self.places[target_x][target_y].moves += 1
-                self.places[piece_x][piece_y] = None
-                return True
+                    self.places[target_x][target_y] = piece_to_move
+                    self.places[target_x][target_y].moves += 1
+                    self.places[piece_x][piece_y] = None
+                    return True
         return False
-
-
-class Piece(metaclass=ABCMeta):
-    def __init__(self, x, y, direction, owner):
-        self.in_play = True
-        self.type = None
-        self.x = x
-        self.y = y
-        self.owner = owner
-        self.direction = direction
-        self.moves = 0
-
-    def __str__(self):
-        return self.type + ": " + self.in_play
-
-    @abstractmethod
-    def valid_move(self, target_x, target_y, current_player):
-        if current_player != self.owner:
-            print("Is that your piece?")
-            return False
-        if self.x == target_x and self.y == target_y:
-            return False
-
-
-class Player:
-    def __init__(self, colour):
-        self.color = colour
-        self.captured_pieces = list()
-
-
-class Pawn(Piece):
-    def __init__(self, x, y, direction, owner):
-        super().__init__(x, y, direction, owner)
-        self.type = "Pawn"
-
-    def valid_move(self, target_x, target_y, current_player):
-        super().valid_move(target_x, target_y, current_player)
-        if target_y != self.y + self.direction and \
-                (target_y != self.y + 2 * self.direction and self.moves == 0):
-            return False
-        if target_x > self.x + 1 or target_x < self.x - 1:
-            return False
-        # Straight move
-        return True
-
-    def __str__(self):
-        return "P"
-
-
-class King(Piece):
-    def __init__(self, x, y, direction, owner):
-        super().__init__(x, y, direction, owner)
-        self.type = "King"
-
-    def valid_move(self, target_x, target_y, current_player):
-        super().valid_move(target_x, target_y, current_player)
-        if self.x - 1 <= target_x <= self.x + 1 and self.y - 1 <= target_y <= self.y + 1:
-            return True
-        else:
-            # Check for castling
-            return False
-
-    def __str__(self):
-        return "K"
-
-
-class Queen(Piece):
-    def __init__(self, x, y, direction, owner):
-        super().__init__(x, y, direction, owner)
-        self.type = "Queen"
-
-    def valid_move(self, target_x, target_y, current_player):
-        super().valid_move(target_x, target_y, current_player)
-        if (target_x - self.x) % (target_y - self.y) == 0 or \
-                        target_y == self.y or \
-                        target_x == self.x:
-            return True
-        else:
-            return False
-
-    def __str__(self):
-        return "Q"
-
-
-class Bishop(Piece):
-    def __init__(self, x, y, direction, owner):
-        super().__init__(x, y, direction, owner)
-        self.type = "Bishop"
-
-    def valid_move(self, target_x, target_y, current_player):
-        super().valid_move(target_x, target_y, current_player)
-        if (target_x - self.x) % (target_y - self.y) == 0:
-            return True
-        else:
-            return False
-
-    def __str__(self):
-        return "B"
-
-
-class Knight(Piece):
-    def __init__(self, x, y, direction, owner):
-        super().__init__(x, y, direction, owner)
-        self.type = "Knight"
-
-    def valid_move(self, target_x, target_y, current_player):
-        super().valid_move(target_x, target_y, current_player)
-        if (abs(target_x - self.x) == 2 and abs(target_y - self.y) == 3) or \
-                (abs(target_x - self.x) == 3 and abs(target_y - self.y) == 3):
-            return True
-        else:
-            return False
-
-    def __str__(self):
-        return "H"
-
-
-class Rook(Piece):
-    def __init__(self, x, y, direction, owner):
-        super().__init__(x, y, direction, owner)
-        self.type = "Rook"
-
-    def valid_move(self, target_x, target_y, current_player):
-        super().valid_move(target_x, target_y, current_player)
-        if target_y == self.y or target_x == self.x:
-            return True
-        else:
-            return False
-
-    def __str__(self):
-        return "R"
 
 
 class Game:
@@ -242,7 +231,7 @@ class Game:
         self.players = [player1, player2]
 
     def move(self):
-        instruction = input("Player {} to Move: ".format(self.current_player))
+        instruction = input("{} to Move: ".format(self.players[self.current_player].colour))
         if instruction == 'q':
             exit(0)
         if len(instruction) == 7:
